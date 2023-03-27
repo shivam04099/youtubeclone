39e1e115c2085342dde661328b34488035d38a7e
shivam04099 <115458007+shivam04099@users.noreply.github.com>
2023-03-27 22:27:39 +0530

    Add files via upload

---
+++ src/utils/Api.js
@@ -0,0 +1,18 @@
+//import axios from "axios";
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key":
+        '898589ec21msh429d44e9bafa85ep1cb604jsn0d8719550736',
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
